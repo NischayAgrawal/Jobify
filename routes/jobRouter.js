b106506ecf64b558f4c71a1521849af72dc01a5c
NischayAgrawal <nischay2001agrawal@gmail.com>
2024-12-17 17:19:49 +0530

    implemented chart feature to get the stats of a user

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 const router = Router();
 
-import { getAllJobs, getSingleJob, createJob, updateJob, deleteJob } from "../controllers/jobController.js";
+import { getAllJobs, getSingleJob, createJob, updateJob, deleteJob, showStats } from "../controllers/jobController.js";
 import { validationJobInput, validateIdParam } from "../middlewares/validationMiddleware.js";
 import { checkForTestUser } from "../middlewares/authMiddleware.js";
 
 router.route("/").get(getAllJobs).post(checkForTestUser, validationJobInput, createJob);
+
+router.route("/stats").get(showStats);
+
 router.route("/:id").get(validateIdParam, getSingleJob).patch(checkForTestUser, validationJobInput, validateIdParam, updateJob).delete(checkForTestUser, validateIdParam, deleteJob);
 
 export default router;
